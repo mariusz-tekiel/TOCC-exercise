3c0491ce3f5d5687e0c08a2776f6b7446bef065f
MTekiel <mtekiel777@gmail.com>
2021-09-02 13:08:51 +0100

    Create web form

---
+++ 4-webpage/form.php
@@ -0,0 +1,25 @@
+<?php include("head.php"); ?>
+
+<body>
+    <h1>WWW.SUFFOLK-HOLIDAYS.CO.UK</h1>
+    <div class="container">
+        <h2>Want to buy this domain?</h2>
+        <form action="show_data.php">
+            <div class="form-group">      
+                <input type="text" class="form-control" id="title" placeholder="Title" name="title">
+            </div>
+            <div class="form-group">      
+                <input type="text" class="form-control" id="pwd" placeholder="Surname" name="surname">
+            </div>
+            <div class="form-group">      
+                <input type="email" class="form-control" id="email" placeholder="Email" name="email">
+            </div>
+            <div class="form-group">      
+                <input type="text" class="form-control" id="message" placeholder="Message" name="message">
+            </div>
+            
+            <button type="submit" class="btn btn-default">Enquire now</button>
+        </form>
+    </div>
+</body>
+</html>
