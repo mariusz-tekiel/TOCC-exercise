4de3ce4f927af17e00a4fe38af9d510ed7e65b28
MTekiel <mtekiel777@gmail.com>
2021-09-07 15:58:19 +0100

    Syntax correction 2

--- 4-5-webpage/head.php
+++ 4-5-webpage/head.php
@@ -8,3 +8,4 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 </head>
+
